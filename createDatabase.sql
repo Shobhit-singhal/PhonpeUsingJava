c54abaa2acc4cd569916a434252e497a18fa1990
shobhit singhal <singhalshobhit12@gmail.com>
2025-04-16 18:53:47 +0530

    someChanges

--- createDatabase.sql
+++ createDatabase.sql
@@ -3,17 +3,17 @@ create database if not exists phonpe_final;
 use phonpe_final;
 -- 1. BANK ACCOUNT
 CREATE TABLE bank_account (
-    acc_no INT AUTO_INCREMENT PRIMARY KEY,
+    acc_no int AUTO_INCREMENT PRIMARY KEY,
     bankname VARCHAR(100) NOT NULL,
     IFSC_Code VARCHAR(20) NOT NULL,
-    balance DECIMAL(12, 2) default 0 NOT NULL,
+    balance DECIMAL(12, 2) DEFAULT 0 NOT NULL,
     uid INT NOT NULL
 );
 
 -- 2. UPI
 CREATE TABLE upi (
     upi_id int AUTO_INCREMENT PRIMARY KEY,
-    acc_no BIGINT NOT NULL,
+    acc_no int NOT NULL,
     FOREIGN KEY (acc_no) REFERENCES bank_account(acc_no)
 );
 
@@ -22,8 +22,8 @@ CREATE TABLE user (
     uid INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
-    address TEXT,
-    default_upi_id VARCHAR(100),
+    address VARCHAR(100),
+    default_upi_id INT,
     FOREIGN KEY (default_upi_id) REFERENCES upi(upi_id)
 );
 
@@ -60,7 +60,7 @@ CREATE TABLE transaction (
 -- 7. USES_UPI
 CREATE TABLE uses_upi (
     t_id INT NOT NULL,
-    upi_id VARCHAR(100) NOT NULL,
+    upi_id INT NOT NULL,
     type ENUM('credit', 'debit') NOT NULL,
     PRIMARY KEY (t_id, upi_id),
     FOREIGN KEY (t_id) REFERENCES transaction(t_id),
